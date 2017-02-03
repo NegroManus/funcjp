d45a065bf43c004a820520aacdc48e0c48ebcfba
negromanus <negromanus90@gmail.com>
2017-02-03 15:30:06 +0100

    corrected functools.py

--- funcp/src/de/fuh/seminar1908/funcjp/functools.py
+++ funcp/src/de/fuh/seminar1908/funcjp/functools.py
@@ -2,7 +2,7 @@ import functools
 import operator
 
 faculty_10 = functools.reduce(operator.mul, range(1, 11))
-print(faculty_10)
+print("10! =", faculty_10)
 
 add10 = functools.partial(operator.add, 10)
 print("10 + 5 =", add10(5))
