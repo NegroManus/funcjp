1dda25bfe9ebeabb144df70f2dce3d2182be227a
negromanus <negromanus90@gmail.com>
2017-03-31 09:57:49 +0200

    Extended code_as_data.py examle with an lambda expr;

--- funcp/src/de/fuh/seminar1908/funcjp/code_as_data.py
+++ funcp/src/de/fuh/seminar1908/funcjp/code_as_data.py
@@ -1,9 +1,14 @@
 from threading import Thread
 import threading
 
+
 def run():
     # Ich laufe in  <Thread(Thread-1, started 10024)>
     print('Ich laufe in ', threading.current_thread())
 
 thread = Thread(target=run)
+thread.start()
+
+thread = Thread(
+    target=lambda: print('Ich laufe in ', threading.current_thread()))
 thread.start()
